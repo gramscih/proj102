83df0e49f5190b508ddc449b0c29dac87c21027e
gramsci <gramscihc@gmail.com>
2023-01-16 16:17:05 -0500

    First Class

---
+++ class1.py
@@ -0,0 +1,46 @@
+# Variable
+
+msg = "this is an string"
+
+full_name = "Gramsci Hermozo"
+
+msg_1 = "other thing"
+# 2_msg = "other thing" # wrong
+
+message = 2
+
+msg = 2
+
+# Operators
+
+sum1 = 2 + 3
+sub = 5 - 2
+mul = 2 * 3
+div = 10 / 2
+exp = 4 ** 2
+mod = 7 % 3
+div_ent = 10 // 2
+
+# Inplace operators
+
+sum1 = sum1 + 1
+sum1 += 1
+sub -= 1
+mul *= 2
+
+# Boolean Operators
+x = True
+y = False
+z = x and y
+v = x or y
+n = not x
+
+print(z, v, n)
+
+x = 2
+y = 0
+z = x and y
+v = x or y
+n = not x
+
+print(z, v, n)
